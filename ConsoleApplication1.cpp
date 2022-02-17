7fded8d86cad8991863fdd8bbf61370546226d85
123 <s.y.r.v@mail.ru>
2022-02-17 16:00:27 +0500

    Add project files.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+void print()
+{
+    std::cout << "Hello SkillBox!\n";
+}
+int main()
+{
+    std::cout << "Hello SkillBox!\n";
+    int x = 100;
+    int y = x + 100;
+    int mult = x * y;
+
+
+    int random = 1004;
+    std::cout << mult;
+    std::cout << '\n';
+    std::cout << random;
+}
+
